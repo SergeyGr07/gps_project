50b592ed66f0ed553ca5ceec4a0901cf326f1111
SergeyGr07 <49278850+SergeyGr07@users.noreply.github.com>
2023-04-10 16:42:19 +0800

    table_data

--- tracker/static/com.py
+++ tracker/static/com.py
@@ -1,10 +1,11 @@
-import serial
+def reading_file():
+    st = [x.split(' ') for x in open('D:/coding/gps_project/data.txt').readlines()]
+    datas = [x for x in st]
+    message1 = " ".join(datas[14])
+    message2 = " ".join(datas[15])
+    message3 = " ".join(datas[16])
+    messages = message1 + message2 + message3
+    return messages
 
-# Указываем имя COM-порта и скорость передачи данных
-ser = serial.Serial('COM4', 9600)
 
-# Отправляем сообщение в COM-порт
-ser.write(b'Hello, world!')
-
-# Закрываем соединение
-ser.close()
+messages = reading_file()
